f282c7cf14149d7c75538f2e2c96f8b19dc2f7e8
Yuhang_warehouse <494977436@qq.com>
2026-09-23 10:37:48 +0800

    回顾9月13日的基础变量赋值和计算。

---
+++ C_Language_project_2026_9_23/C_Language_project_2026_9_23/C_Language_project_2026_9_23/test1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = 0;
+	scanf("%d", &c);
+	int d = a * c + b;
+	printf("%d\n", d);
+
+	return 0;
+}
